09777fe2e6f571690e5721e6290576d2babb954d
Karakure178 <egg9017shun@gmail.com>
2024-07-28 23:31:35 +0900

    add - パス追加/画像追加/README更新

---
+++ pages/blog/news/[...slug].vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <NuxtLayout>
+    <ContentDoc v-slot="{ doc }">
+      <ArticleLayout>
+        <template #title>
+          <Headline>{{ doc.title }}</Headline>
+        </template>
+
+        <template #content>
+          <ContentRenderer :value="doc" />
+        </template>
+      </ArticleLayout>
+    </ContentDoc>
+  </NuxtLayout>
+</template>
