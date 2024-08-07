0c0a895d3399d85dec4736b2c5e7a84c6e022f78
Karakure178 <egg9017shun@gmail.com>
2024-08-08 00:32:15 +0900

    fix - tsに一部書き換え

--- components/Common/Header/Header.vue
+++ components/Common/Header/Header.vue
@@ -1,29 +1,10 @@
-<script setup>
-const navList = [
-  { id: 1, title: "Home", link: "/home" },
-  { id: 2, title: "Blog", link: "/blog" },
-  { id: 3, title: "Works", link: "/works" },
-  { id: 4, title: "Contact", link: "/contact" },
-];
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <header class="header">
     <div class="header__inner">
       <CommonHeaderLogo />
-      <nav class="header__nav">
-        <ul class="header__navList">
-          <li
-            v-for="item of navList"
-            :key="item.title"
-            class="headerNavList__item"
-          >
-            <a class="headerNavListItem__link" :href="item.link">{{
-              item.title
-            }}</a>
-          </li>
-        </ul>
-      </nav>
+      <CommonHeaderNav />
     </div>
   </header>
 </template>
@@ -52,35 +33,6 @@ const navList = [
     padding-top: 14px;
     padding-bottom: 14px;
     padding-inline: 15px;
-  }
-}
-
-.header__nav {
-}
-
-.header__navList {
-  display: flex;
-  gap: 16px;
-}
-
-.headerNavList__item {
-}
-
-.headerNavListItem__link {
-  font-size: 16px;
-  font-family: $roboto;
-  color: $color-neutral_dark-content;
-  opacity: 1;
-  transition: opacity 0.3s;
-
-  @include L-XL {
-  }
-
-  @include S-M {
-  }
-
-  @include hover-and-active {
-    opacity: 0.8;
   }
 }
 </style>
---
+++ components/Common/Header/Nav/Nav.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+interface NavList {
+  id: number;
+  title: string;
+  link: string;
+}
+const navList: NavList[] = [
+  { id: 1, title: "Home", link: "/home" },
+  { id: 2, title: "Blog", link: "/blog" },
+  { id: 3, title: "Works", link: "/works" },
+  { id: 4, title: "Contact", link: "/contact" },
+];
+</script>
+
+<template>
+  <nav class="headerNav">
+    <ul class="headerNavList">
+      <li
+        v-for="(item, index) of navList"
+        :key="`navList--${index}`"
+        class="headerNavList__item"
+      >
+        <a class="headerNavListItem__link" :href="item.link">{{
+          item.title
+        }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+// ===========================================
+// nav 全体設定
+// ===========================================
+.headerNav {
+}
+
+.headerNavList {
+  display: flex;
+  gap: 16px;
+}
+
+.headerNavList__item {
+}
+
+.headerNavListItem__link {
+  font-size: 16px;
+  font-family: $roboto;
+  color: $color-neutral_dark-content;
+  opacity: 1;
+  transition: opacity 0.3s;
+
+  @include L-XL {
+  }
+
+  @include S-M {
+  }
+
+  @include hover-and-active {
+    opacity: 0.8;
+  }
+}
+</style>
